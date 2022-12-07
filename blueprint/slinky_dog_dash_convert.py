e4751409197c270a5ff377f726b9fa28713a59c4
smjohnsonShipyard <104784108+smjohnsonShipyard@users.noreply.github.com>
2022-12-07 11:10:22 -0500

    Update slinky_dog_dash_convert.py

--- blueprint/slinky_dog_dash_convert.py
+++ blueprint/slinky_dog_dash_convert.py
@@ -3,3 +3,7 @@ import os
 
 file_name = os.environ.get('file_name')
 exported_file_name = os.environ.get('exported_file_name')
+
+df = pd.read_csv(file_name)
+df['SPOSTHR'] = round(df['SPOSTMIN'] / 60,2)
+df.to_csv(exported_file_name,index = None)
